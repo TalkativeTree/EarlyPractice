fe6407581eb15217604279cdd00a588997a1f866
Ben Angel <benjamin.b2@gmail.com>
2013-03-26 21:55:48 -0700

    Find the longest string in an array

---
+++ longest_string.rb
@@ -0,0 +1,14 @@
+# longest_string is a method that takes an array of strings as its input
+# and returns the longest string
+#
+# +array+ is an array of strings
+# longest_string(array) should return the longest string in +array+
+#
+# If +array+ is empty the method should return nil
+
+def longest_string(array)
+  array.map{|x, v| v = x.size; x}.sort.last
+
+end
+
+puts longest_string(['cat', 'zzzzzzz', 'apples'])
