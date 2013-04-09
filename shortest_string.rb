c76a8d44058ca1cba050f480c0afa153c743d0a8
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2013-04-09 02:26:20 -0700

    updates

--- shortest_string.rb
+++
@@ -1,7 +0,0 @@
-
-
-def longest_string(array)
-   array.min_by {|x| x.length } 
-end
-array = ["aaaaaaaaaa", "aaaaaaaa", "aaaaa", "aaaaaaa", "aaaaaaaaa", "aaa", "a", "aa", "aaaaaa", "aaaa"]
-puts longest_string([array])
