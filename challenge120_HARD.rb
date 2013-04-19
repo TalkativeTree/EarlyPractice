7641179ae6fd56b937d8b9df42a19ec18654a48f
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2013-04-19 04:09:53 -0700

    reddit challenge 120 HARD

---
+++ challenge120_HARD.rb
@@ -0,0 +1,44 @@
+class SittingDucks
+
+	def initialize filename
+		@filename 		= filename
+		@mortal_enemies = []
+		@table_1 		= []
+		@table_2 		= []
+		@guest_list 	= []
+	end
+
+	def organize_lists
+		file = File.open(@filename)		
+		file.each {|line| @mortal_enemies << line.chomp.split(",")}	
+		@guest_list = Hash.new
+		@mortal_enemies.each_with_index{|guest, hates| @guest_list[hates] = guest }
+		@table_1 << @guest_list.fetch(0)
+	end
+
+	def seat_enemies 
+		organize_lists
+		@guest_list.each_key { |key| 
+			if @table_1 == @table_2
+				puts "fuck just cancel it"
+				return "You're gonna need an extra table" 
+			elsif @table_1.include?((key).to_s)				
+				@table_1.map { |guest| @table_2 << @guest_list.fetch(guest.to_i - 1) }
+			elsif	@table_2.include?((key).to_s)				
+				@table_2.map { |guest| @table_1 << @guest_list.fetch(guest.to_i - 1) }
+			end	
+		@table_2 = @table_2.flatten.uniq.sort
+		@table_1 = @table_1.flatten.uniq.sort
+		}
+		puts "Congrats on your successful dinner party. Here are the seating"
+		puts "arrangements for your dinner party."
+		puts @table_1
+		puts 
+		puts @table_2
+	end
+end
+
+		
+
+seating_arrangement = SittingDucks.new("mortal_enemies.txt")
+seating_arrangement.seat_enemies
